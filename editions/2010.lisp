090d93ae51e6eb5b3faf63a8691cec77cb5b94f8
Shinmera <shinmera@tymoon.eu>
2016-12-14 23:35:48 +0100

    Stubs for all previous editions.

---
+++ editions/2010.lisp
@@ -0,0 +1,62 @@
+(in-package #:els-web-editions)
+(define-edition "2010")
+(in-package "2010")
+
+;;; Texts
+(define-text :announcement
+  (:div :class "announcement" "The Conference is Over!"))
+
+(define-date "Conference start" @2010-05-06)
+
+(define-date "Conference end" @2010-05-07)
+
+;;; Sponsors
+;; (define-sponsor epita
+;;   :website "http://www.epita.fr/"
+;;   :logo "http://www.epita.fr/images/logo-epita.png?v=26112014")
+
+;; (define-sponsor lispworks
+;;   :website "http://www.lispworks.com/"
+;;   :logo "http://www.lispworks.com/images/logo.png")
+
+;; (define-sponsor franz
+;;   :website "http://www.franz.com/"
+;;   :logo "http://franz.com/images/logo.png")
+
+;; (define-sponsor clozure
+;;   :website "http://www.clozure.com/"
+;;   :logo "http://www.clozure.com/images/clozure-logo.png")
+
+;; (define-sponsor google
+;;   :website "http://www.itasoftware.com/"
+;;   :logo "http://www.itasoftware.com/images/ita-logo-md.png")
+
+;;; People
+(define-person (:family-name "Verna"
+                :given-name "Didier")
+  :role (:administrator)
+  :email "didier@elsaa.org"
+  :website "http://www.didierverna.net"
+  :organization "ELS")
+
+;;; Locations
+;; (define-location "Somewhere"
+;;   :role (:conference)
+;;   :photo "IMAGE URL"
+;;   :website "WEBSITE URL"
+;;   :address (:street "A"
+;;             :extended "B"
+;;             :post-code "C"
+;;             :city "D"
+;;             :country "E")
+;;   :map T)
+
+;;; Programme
+;; (define-programme-day @2010-05-06T00:00:00+ZZ:ZZ
+;;   @08:30:00 (:title "Registration"
+;;              :role (:organization))
+;;   @09:00:00 (:title "Welcome Message"
+;;              :role (:organization))
+;;   ...
+;;   @19:30:00 (:title "Conference Dinner"
+;;              :role (:dinner)))
---
+++ editions/2011.lisp
@@ -0,0 +1,62 @@
+(in-package #:els-web-editions)
+(define-edition "2011")
+(in-package "2011")
+
+;;; Texts
+(define-text :announcement
+  (:div :class "announcement" "The Conference is Over!"))
+
+(define-date "Conference start" @2011-03-31)
+
+(define-date "Conference end" @2011-04-01)
+
+;;; Sponsors
+;; (define-sponsor epita
+;;   :website "http://www.epita.fr/"
+;;   :logo "http://www.epita.fr/images/logo-epita.png?v=26112014")
+
+;; (define-sponsor lispworks
+;;   :website "http://www.lispworks.com/"
+;;   :logo "http://www.lispworks.com/images/logo.png")
+
+;; (define-sponsor franz
+;;   :website "http://www.franz.com/"
+;;   :logo "http://franz.com/images/logo.png")
+
+;; (define-sponsor clozure
+;;   :website "http://www.clozure.com/"
+;;   :logo "http://www.clozure.com/images/clozure-logo.png")
+
+;; (define-sponsor google
+;;   :website "http://www.itasoftware.com/"
+;;   :logo "http://www.itasoftware.com/images/ita-logo-md.png")
+
+;;; People
+(define-person (:family-name "Verna"
+                :given-name "Didier")
+  :role (:administrator)
+  :email "didier@elsaa.org"
+  :website "http://www.didierverna.net"
+  :organization "ELS")
+
+;;; Locations
+;; (define-location "Somewhere"
+;;   :role (:conference)
+;;   :photo "IMAGE URL"
+;;   :website "WEBSITE URL"
+;;   :address (:street "A"
+;;             :extended "B"
+;;             :post-code "C"
+;;             :city "D"
+;;             :country "E")
+;;   :map T)
+
+;;; Programme
+;; (define-programme-day @2011-03-31T00:00:00+ZZ:ZZ
+;;   @08:30:00 (:title "Registration"
+;;              :role (:organization))
+;;   @09:00:00 (:title "Welcome Message"
+;;              :role (:organization))
+;;   ...
+;;   @19:30:00 (:title "Conference Dinner"
+;;              :role (:dinner)))
