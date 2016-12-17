a9d7782e527ea9a7b38873b81c25242886c6eb0c
Shinmera <shinmera@tymoon.eu>
2016-12-17 10:46:17 +0100

    Minor fixes

--- editions/2017.lisp
+++ editions/2017.lisp
@@ -8,8 +8,9 @@
   (:div :class "announcement" "Submissions are now open!"))
 
 (define-text :submissions
-  (:p "All submissions should be formatted following the <a href=\"http://www.acm.org/sigs/publications/proceedings-templates\">ACM SIGS guidelines</a> and include <a href=\"http://www.acm.org/about/class/1998\">ACM classification categories and terms</a>.")
-  (:p "You can upload submissions on <a href=\"https://www.easychair.org/conferences/?conf=els17\">EasyChair</a>."))
+  (:p "You can upload submissions on <a href=\"https://www.easychair.org/conferences/?conf=els17\">EasyChair</a>.")
+  (:p "All submissions should be formatted following the ACM SIGS guidelines and include <a href=\"http://www.acm.org/about/class/2012\">ACM classification categories and terms</a>. Appropriate TeX and Word templates can be found on the <a href=\"http://www.acm.org/sigs/publications/proceedings-templates\">ACM publications page</a>.")
+  (:p "Please use the <a href=\"http://dl.acm.org/ccs/ccs.cfm\">ACM Computing Classification System site</a> to generate the CCS codes. In order to add a concept, navigate to it using the boxes, and select \"Assign This CCS Concept\" on the left side. You should select one high relevance concept, and up to four medium or low relevance concepts. Once you are done, simply copy the CCS display into the Word document. If you are using TeX, select \"View CCS TeX Code\" and copy the displayed code into your TeX file. The templates should already include sections with bogus CCS codes, which you can simply replace with your own."))
 
 (define-date "Submission deadline" @2017-01-30)
 
