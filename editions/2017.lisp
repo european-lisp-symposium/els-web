4b40b8150155f6a9e2536b73d4a143ce56dc5641
Shinmera <shinmera@tymoon.eu>
2017-03-14 10:21:10 +0100

    Update 2017 edition.

--- editions/2017.lisp
+++ editions/2017.lisp
@@ -5,17 +5,11 @@
 ;;; Texts
 (define-text :announcement
   (:div :class "announcement" "This year's conference is co-located with <a href=\"http://2017.programmingconference.org/home\">Programming 2017</a>")
-  (:div :class "announcement" "Submissions are now open!"))
+  (:div :class "announcement" "Submissions are now closed."))
 
-(define-text :submissions
-  (:p "You can upload submissions on <a href=\"https://www.easychair.org/conferences/?conf=els17\">EasyChair</a>.")
-  (:p "All submissions should be formatted following the ACM SIGS guidelines and include <a href=\"http://www.acm.org/about/class/2012\">ACM Computing Classification System 2012</a> concepts and terms. Appropriate TeX and Word templates can be found on the <a href=\"http://www.acm.org/sigs/publications/proceedings-templates\">ACM publications page</a>.")
-  (:p "Please use the <a href=\"http://dl.acm.org/ccs/ccs.cfm\">ACM Computing Classification System site</a> to generate the CCS codes. In order to add a concept, navigate to it using the boxes, and select \"Assign This CCS Concept\" on the left side. You should select one high relevance concept, and up to four medium or low relevance concepts. Once you are done, simply copy the CCS display into the Word document. If you are using TeX, select \"View CCS TeX Code\" and copy the displayed code into your TeX file. The templates should already include sections with bogus CCS codes, which you can simply replace with your own.")
-  (:p "If you are using the correct style and classification system, your document will contain a section called <code>CCS Concepts</code> and include terms formatted like <code>•Information systems → Web applications</code>. If your document includes a section titled <code>Categories and Subject Descriptors</code> with things in a style like <code>D.2.3 [Software Engineering]: Coding Tools and Techniques</code>, then you have to update to the new 2012 system and templates as linked above. If the CCS section does not show up in the TeX generated PDF at all, make sure that your TeX file includes the <code>\\printccsdesc</code> command below the abstract."))
+(define-date "Submission deadline" @2017-02-06)
 
-(define-date "Submission deadline" @2017-01-30)
-
-(define-date "Notification of acceptance" @2017-02-27)
+(define-date "Notification of acceptance" @2017-03-06)
 
 (define-date "Final papers" @2017-03-20)
 
@@ -27,6 +21,22 @@
 (define-sponsor brunner
   :website "http://www.systemhaus-brunner.de/"
   :logo "../static/logos/brunner.png")
+
+(define-sponsor epita
+  :website "http://www.epita.fr/"
+  :logo "../static/logos/epita.png")
+
+(define-sponsor lispworks
+  :website "http://www.lispworks.com/"
+  :logo "../static/logos/lispworks.png")
+
+(define-sponsor franz
+  :website "http://www.franz.com/"
+  :logo "../static/logos/franz.png")
+
+(define-sponsor cl-foundation
+  :website "http://cl-foundation.org/"
+  :logo "../static/logos/cl-foundation.png")
 
 ;;; People
 (define-person (:family-name "Verna"
@@ -129,6 +139,21 @@
   :organization "Università degli Studi di Genova"
   :address (:country "Italy"))
 
+(define-person (:family-name "Hübner"
+                :given-name "Hans")
+  :role (:speaker)
+  :organization "LambdaWerk GmbH"
+  :address (:country "Germany"
+            :city "Berlin"))
+
+(define-person (:family-name "Khomtchouk"
+                :given-name "Bohdan"
+                :additional-name "B.")
+  :role (:speaker)
+  :organization "University of Miami Miller School of Medicine"
+  :address (:country "USA"
+            :city "Miami"))
+
 ;;; Locations
 (define-location "Vrije Universiteit Brussel"
   :role (:conference)
@@ -142,3 +167,18 @@
   :map T)
 
 ;;; Programme
+(define-programme-day @2017-04-03T00:00:00+01:00
+  @09:30:00 (:title "How the strengths of Lisp-family languages facilitate building complex and flexible bioinformatics applications"
+             :role (:talk :keynote)
+             :speakers ("Bohdan Khomtchouk")
+             :description "We present a rationale for expanding the presence of the Lisp family of programming languages in bioinformatics and computational biology research. Put simply, Lisp-family languages enable programmers to more quickly write programs that run faster than in other languages. Languages such as Common Lisp, Scheme and Clojure facilitate the creation of powerful and flexible software that is required for complex and rapidly evolving domains like biology. We will point out several important key features that distinguish languages of the Lisp family from other programming languages, and we will explain how these features can aid researchers in becoming more productive and creating better code. We will also show how these features make these languages ideal tools for artificial intelligence and machine learning applications. We will specifically stress the advantages of domain-specific languages (DSLs): languages that are specialized to a particular area, and thus not only facilitate easier research problem formulation, but also aid in the establishment of standards and best programming practices as applied to the specific research field at hand. DSLs are particularly easy to build in Common Lisp, the most comprehensive Lisp dialect, which is commonly referred to as the 'programmable programming language'. We are convinced that Lisp grants programmers unprecedented power to build increasingly sophisticated artificial intelligence systems that may ultimately transform machine learning and artificial intelligence research in bioinformatics and computational biology."))
+
+(define-programme-day @2017-04-04T00:00:00+01:00
+  @09:00:00 (:title "Identity in a World of Values"
+             :role (:talk :keynote)
+             :speakers ("Hans Hübner")
+             :description "Data persistence can add a great deal of complexity to application software, and making the gap between application and storage has been a constant field of research, experiments and products. In the object oriented programming paradigm, persistence seems to be a natural extension to object behavior, and even though one could argue that many persistent object systems have flaws and leak their abstractions, there is a large body of prior art and research in that area.
+
+In the functional programming world, persistence does not find as natural a partnering abstraction, and it is often either conceptionally pushed to the boundaries of the application, or treated in an ad-hoc fashion interleaved with the beauty and conceptional rigor of pure functions.
+
+The presentation discusses these forces and explores how Clojure's Software Transactional Memory system can be used to implement application data persistence."))
