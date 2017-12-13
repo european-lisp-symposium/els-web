560176af2f4e32101d94a4fe969313f9600aaa95
Didier Verna <didier@didierverna.net>
2017-12-13 17:13:44 +0100

    Add Dave as the 2018 PC chair.

--- editions/2018.lisp
+++ editions/2018.lisp
@@ -45,6 +45,11 @@
   :website "http://www.european-lisp-symposium.org"
   :organization "ELS")
 
+(define-person (:family-name "Cooper"
+		:given-name "David")
+  :role (:programme-chair)
+  :organization "Genworks Intl.")
+
 (define-person (:family-name "Lawson"
 		:given-name "Andrew")
   :role (:local-chair)
