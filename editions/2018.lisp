b8d7a0733a313ffbdcb9a793a94452ddd7caeec0
Didier Verna <didier@didierverna.net>
2018-04-16 16:11:50 +0200

    [ELS 2018] Deactivate registration.

--- editions/2018.lisp
+++ editions/2018.lisp
@@ -9,7 +9,7 @@
   (:div :class "announcement" "Program announced!")
   ;; (:div :class "announcement" "Invited speakers announced!")
   ;; (:div :class "announcement" "Registrations are closed!")
-  (:div :class "announcement" "Registration is now open!")
+  ;; (:div :class "announcement" "Registration is now open!")
   ;; (:div :class "announcement" "Submissions deadline extended by one week!")
   ;; (:div :class "announcement" "Submissions are now open!")
   ;; (:div :class "announcement" "The conference is being organized. Stay tuned for updates!")
@@ -108,129 +108,129 @@ like this: <code>\\usepackage[format=sigconf]{acmart}</code>." )
 
 ;;; People
 (define-person (:family-name "Verna"
-		:given-name "Didier")
+        :given-name "Didier")
   :role (:administrator :speaker)
   :email "www@european-lisp-symposium.org"
   :website "http://www.european-lisp-symposium.org"
   :organization "ELS")
 
 (define-person (:family-name "Cooper"
-		:given-name "David")
+        :given-name "David")
   :role (:programme-chair)
   :organization "Genworks")
 
 (define-person (:family-name "Lawson"
-		:given-name "Andrew")
+        :given-name "Andrew")
   :role (:local-chair)
   :organization "Ravenpack")
 
 (define-person (:family-name "Durand"
-		:given-name "Irène")
+        :given-name "Irène")
   :role (:committee :speaker)
   :organization "LaBRI, Université de Bordeaux"
   :address (:country "France"))
 
 (define-person (:family-name "McKay"
-		:given-name "Scott")
+        :given-name "Scott")
   :role (:committee)
   :organization "Future Fuel"
   :address (:country "USA"))
 
 (define-person (:family-name "Neuss"
-		:given-name "Nicolas")
+        :given-name "Nicolas")
   :role (:committee :speaker)
   :organization "FAU Erlangen-Nürnberg"
   :address (:country "Germany"))
 
 (define-person (:family-name "Rhodes"
-		:given-name "Christophe")
+        :given-name "Christophe")
   :role (:committee :speaker)
   :organization "Goldsmiths University"
   :address (:country "UK"))
 
 (define-person (:family-name "Padget"
-		:given-name "Julian")
+        :given-name "Julian")
   :role (:committee)
   :organization "University of Bath"
   :address (:country "UK"))
 
 (define-person (:family-name "Strandh"
-		:given-name "Robert")
+        :given-name "Robert")
   :role (:committee :speaker)
   :organization "Université de Bordeaux"
   :address (:country "France"))
 
 (define-person (:family-name "Sperber"
-		:given-name "Michael")
+        :given-name "Michael")
   :role (:committee)
   :organization "DeinProgramm"
   :address (:country "Germany"))
 
 (define-person (:family-name "Courtès"
-		:given-name "Ludovic")
+        :given-name "Ludovic")
   :role (:committee)
   :organization "Inria"
   :address (:country "France"))
 
 (define-person (:family-name "Chua"
-		:given-name "Sacha")
+        :given-name "Sacha")
   :role (:committee)
   :organization "Living an Awesome Life"
   :address (:country "Canada"))
 
 (define-person (:family-name "Wellons"
-		:given-name "Christopher")
+        :given-name "Christopher")
   :role (:committee)
   :organization "Null Program"
   :address (:country "USA"))
 
 (define-person (:family-name "Shivers"
-		:given-name "Olin")
+        :given-name "Olin")
   :role (:committee)
   :organization "Northeastern University"
   :address (:country "USA"))
 
 (define-person (:family-name "Wingo"
-        	:given-name "Andy")
+            :given-name "Andy")
   :role (:committee)
   :organization "Igalia"
   :address (:country "Spain"))
 
 (define-person (:family-name "Marek"
-		:given-name "Philipp")
+        :given-name "Philipp")
   :role (:committee)
   :address (:country "Austria"))
 
 (define-person (:family-name "van Waning"
-		:given-name "Ernst")
+        :given-name "Ernst")
   :organization "Infometrics.nl"
   :role (:committee)
   :address (:country "Netherlands"))
 
 (define-person (:family-name "Emerson"
-		:given-name "R. Matthew")
+        :given-name "R. Matthew")
   :role (:speaker)
   :address (:country "USA"))
 
 (define-person (:family-name "Domkin"
-		:given-name "Vsevolod")
+        :given-name "Vsevolod")
   :role (:speaker)
   :address (:country "Ukraine"))
 
 (define-person (:family-name "Heisig"
-		:given-name "Marco")
+        :given-name "Marco")
   :role (:speaker))
 
 (define-person (:family-name "Schwartz"
-		:given-name "Ethan")
+        :given-name "Ethan")
   :role (:speaker))
 
 (define-person (:family-name "Newton"
-		:given-name "Jim")
+        :given-name "Jim")
   :role (:speaker))
 
 (define-person (:family-name "Smith"
-		:given-name "Robert")
+        :given-name "Robert")
   :role (:speaker))
 
 (define-person (:family-name "Tezak"
@@ -238,35 +238,35 @@ like this: <code>\\usepackage[format=sigconf]{acmart}</code>." )
   :role (:speaker))
 
 (define-person (:family-name "Wood"
-		:given-name "Alex")
+        :given-name "Alex")
   :role (:speaker))
 
 (define-person (:family-name "Schafmeister"
-		:given-name "Christian")
+        :given-name "Christian")
   :role (:speaker))
 
 (define-person (:family-name "Jayaprakash"
-		:given-name "Rajesh")
+        :given-name "Rajesh")
   :role (:speaker))
 
 (define-person (:family-name "Hafner"
-		:given-name "Nicolas")
+        :given-name "Nicolas")
   :role (:speaker))
 
 (define-person (:family-name "Ferreira"
-		:given-name "Bruno")
+        :given-name "Bruno")
   :role (:speaker))
 
 (define-person (:family-name "Menezes Leitão"
-		:given-name "António")
+        :given-name "António")
   :role (:speaker))
 
 (define-person (:family-name "Cornez"
-		:given-name "Jason")
+        :given-name "Jason")
   :role (:speaker))
 
 (define-person (:family-name "van Schelven"
-		:given-name "Klaas")
+        :given-name "Klaas")
   :role (:speaker))
 
 
@@ -276,9 +276,9 @@ like this: <code>\\usepackage[format=sigconf]{acmart}</code>." )
   :photo "http://www.marbella.es/cultura/media/k2/galleries/114/museo_cortijo_miraflores_1098.jpg"
   :website "http://www.marbella.es/cultura/centros/item/35-c-c-cortijo-de-miraflores.html"
   :address (:street "Calle San Pedro Alcantara, 10"
-	    :post-code "29601"
-	    :city "Marbella"
-	    :country "Spain")
+        :post-code "29601"
+        :city "Marbella"
+        :country "Spain")
   :map T)
 
 (define-location "Hotel Fuerte (****)"
@@ -294,7 +294,7 @@ like this: <code>\\usepackage[format=sigconf]{acmart}</code>." )
 
 
 ;; Registration
-(define-registration (:active)
+#+()(define-registration (:active)
   (:kind "Early regular" :price 120 :status :inactive
    :description "Full conference package (talks, coffee breaks, banquet).")
   (:kind "Early student" :price 60 :status :inactive
@@ -321,13 +321,13 @@ like this: <code>\\usepackage[format=sigconf]{acmart}</code>." )
   ;;   @08:30:00 (:title "Registration"
   ;;              :role (:organization))
   @09:00:00 (:title "Breakfast on Patio (Coffee, biscuits, bread, fruit)"
-	     :role (:organization))
+         :role (:organization))
   @09:30:00 (:title "Welcome Message" :role (:organization))
 
   @09:40:00 (:title "Keynote: This Old Lisp"
-	     :role (:talk :keynote)
-	     :speakers ("R. Matthew Emerson")
-	     :description "Lisp was invented 60 years ago.  Coral Common Lisp, the ancestor of today's Clozure Common Lisp, was released over 30 years ago.
+         :role (:talk :keynote)
+         :speakers ("R. Matthew Emerson")
+         :description "Lisp was invented 60 years ago.  Coral Common Lisp, the ancestor of today's Clozure Common Lisp, was released over 30 years ago.
 
 Over this time, processor architectures and operating systems have come and gone, but Clozure CL (under various names and forms) has survived and is still with us today.
 
@@ -338,40 +338,40 @@ I will say a few words, looking down from the giant's shoulders, on the subject 
 
 
   @10:30:00 (:title "Petalisp: A Common Lisp Library for Data Parallel Programming"
-	     :role (:talk)
-	     :speakers ("Marco Heisig"))
+         :role (:talk)
+         :speakers ("Marco Heisig"))
 
   @11:00:00 (:title "Dynamic Optimizations for SBCL Garbage Collection"
-	     :role (:talk)
-	     :speakers ("Ethan Schwartz"))
+         :role (:talk)
+         :speakers ("Ethan Schwartz"))
 
   @11:30:00 (:title "Patio Break" :role (:break))
 
   @12:00:00 (:title "Incremental Parsing of Common Lisp Code"
-	     :role (:talk)
-	     :speakers ("Irène Durand" "Robert Strandh"))
+         :role (:talk)
+         :speakers ("Irène Durand" "Robert Strandh"))
 
 
   @12:40:00 (:title "Strategies for Typecase Optimization"
-	     :role (:talk)
-	     :speakers ("Jim Newton" "Didier Verna"))
+         :role (:talk)
+         :speakers ("Jim Newton" "Didier Verna"))
 
 
   @13:20:00 (:title "Method Combinators"
-	     :role (:talk)
-	     :speakers ("Didier Verna"))
+         :role (:talk)
+         :speakers ("Didier Verna"))
 
   @14:00:00 (:title "Lunch" :role (:break))
 
   @15:30:00 (:title "The Computational Structure of the Clifford
   Groups, Simulating Quantum Processor Errors, and Overview of Quantum
   Computing Development with Common Lisp"
-	     :role (:talk)
-  	     :speakers ("Robert Smith" "Nikolas Tezak"))
+         :role (:talk)
+         :speakers ("Robert Smith" "Nikolas Tezak"))
 
   @16:15:00  (:title "Clasp Common Lisp Implementation and Optimization"
-	      :role (:talk)
-	      :speakers ("Alex Wood" "Christian Schafmeister"))
+          :role (:talk)
+          :speakers ("Alex Wood" "Christian Schafmeister"))
 
   @17:00:00  (:title "Patio Break" :role (:break))
 
@@ -382,66 +382,66 @@ I will say a few words, looking down from the giant's shoulders, on the subject 
   @18:45:00  (:title "Travel & Prepare for Dinner")
 
   @19:45:00 (:title "Conference Dinner"
-	     :role (:dinner :organizationl)))
+         :role (:dinner :organizationl)))
 
 
 (define-programme-day @2018-04-17T00:00:00+02:00
   ;;   @08:30:00 (:title "Registration"
   ;;              :role (:organization))
   @09:00:00 (:title "Breakfast on Patio (Coffee, biscuits, bread, fruit)"
-	     :role (:organization))
+         :role (:organization))
   @09:30:00 (:title "Welcome Message" :role (:organization))
 
   @09:40:00 (:title "Keynote: Lisp in a Startup: the Good, the Bad, and the Ugly"
-	     :role (:talk :keynote)
-	     :speakers ("Vsevolod Domkin")
-	     :description "Over the last 10 years of my software development career, I have mostly programmed in Common Lisp, in two distinct environments: open-source and startup (aka consumer Internet companies). Among the startup projects, in which I managed to introduce Lisp, the most successful is Grammarly where the system we had built continues to playa major role — more than two years after my departure from the company — at the core of its intelligent product used by 10 million people daily.
+         :role (:talk :keynote)
+         :speakers ("Vsevolod Domkin")
+         :description "Over the last 10 years of my software development career, I have mostly programmed in Common Lisp, in two distinct environments: open-source and startup (aka consumer Internet companies). Among the startup projects, in which I managed to introduce Lisp, the most successful is Grammarly where the system we had built continues to playa major role — more than two years after my departure from the company — at the core of its intelligent product used by 10 million people daily.
 
 In this talk, I'd like to share the success stories of developing a number of internet services in Lisp and the merits of the Lisp enviornment that enabled those, as well as the flip sides of the same stories that manifest the problems of the Lisp ecosystem — and propose possible solutions to them. We'll discuss where Lisp fits best and worst among the different professional environments and why.")
 
 
   @10:30:00 (:title "pLisp: a Friendly Lisp IDE for Beginners"
-	     :speakers ("Rajesh Jayaprakash")
-	     :role (:talk))
+         :speakers ("Rajesh Jayaprakash")
+         :role (:talk))
 
   @11:00:00 (:title "Using Lisp-based Pseudocode to Probe Student Understanding"
-	     :speakers ("Christophe Rhodes")
-	     :role (:talk))
+         :speakers ("Christophe Rhodes")
+         :role (:talk))
 
   @11:30:00 (:title "Patio Break" :role (:break))
 
   @12:00:00 (:title "Interactive Flow Simulation with Common Lisp"
-	     :speakers ("Nicolas Neuss")
-	     :role (:talk))
+         :speakers ("Nicolas Neuss")
+         :role (:talk))
 
   @12:30:00 (:title "Object-oriented Shader Compositon using CLOS"
-	     :speakers ("Nicolas Hafner")
-	     :role (:talk))
+         :speakers ("Nicolas Hafner")
+         :role (:talk))
 
   @13:00:00 (:title "Context-oriented Algorithmic Design"
-	     :speakers ("Bruno Ferreira" "António Menezes Leitão")
-	     :role (:talk))
+         :speakers ("Bruno Ferreira" "António Menezes Leitão")
+         :role (:talk))
 
   @13:30:00 (:title "Event Detection in Unstructured Text (using Common Lisp)"
-	     :role (:talk :keynote)
-	     :speakers ("Jason Cornez")
-	     :description "At RavenPack, we use Common Lisp to extract meaning from unstructured English text.  The focus is low-latency processing of real-time news feeds and blogs, to provide actionable intelligence to our clients in the financial industry.  This talk discusses our technology for detecting events.  We look at what we've done so far, what we are working on now, and some future possibilities.")
+         :role (:talk :keynote)
+         :speakers ("Jason Cornez")
+         :description "At RavenPack, we use Common Lisp to extract meaning from unstructured English text.  The focus is low-latency processing of real-time news feeds and blogs, to provide actionable intelligence to our clients in the financial industry.  This talk discusses our technology for detecting events.  We look at what we've done so far, what we are working on now, and some future possibilities.")
 
 
   @14:00:00 (:title "Lunch" :role (:break))
 
   @15:30:00 (:title "Clef Design: Thoughts on the Formalization of Program Construction"
-	     :speakers ("Klaas van Schelven")
-	     :role (:talk))
+         :speakers ("Klaas van Schelven")
+         :role (:talk))
 
   @16:15:00 (:title "Partial Inlining using Local Graph Rewriting"
-	     :speakers ("Irène Durand" "Robert Strandh")
-	     :role (:talk))
+         :speakers ("Irène Durand" "Robert Strandh")
+         :role (:talk))
 
   @17:00:00 (:title "Patio Break" :role (:break))
 
   @17:30:00 (:title "Lightning Talks"
-	     :role (:talk))
+         :role (:talk))
 
   @18:00:00 (:title "Common Lisp Foundation Overview & Brainstorming for common-lisp.net Content and Fundraisers" :role (:organization))
 
