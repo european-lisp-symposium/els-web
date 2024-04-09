de684e0e7a6d5a2661be8fc90be9f3023073db58
Didier Verna <didier@didierverna.net>
2024-04-09 13:13:31 +0200

    [2024] Update dates.

--- editions/2024.lisp
+++ editions/2024.lisp
@@ -3,8 +3,8 @@
 (in-package "2024")
 
 #+()(define-date "Submission deadline ** EXTENDED ** " @2024-02-25)
-(define-date "Author notification" @2024-03-24)
-(define-date "Final papers" @2024-04-14)
+#+()(define-date "Author notification" @2024-03-24)
+(define-date "Final papers" @2024-04-21)
 (define-date "Early registration deadline" @2024-04-14)
 
 (define-date "Conference start" @2024-05-06)
