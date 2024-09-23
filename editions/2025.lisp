c12c7ac783061635f60f0f376f6eed4c8c707a10
Didier Verna <didier@didierverna.net>
2024-09-23 11:55:08 +0200

    [2025] Update to the CfP.

--- editions/2025.lisp
+++ editions/2025.lisp
@@ -42,7 +42,8 @@
        presentations about topics of special interest.")
        (:li "<b>Demonstrations</b>: Abstracts of up to 4 pages for demonstrations of tools, libraries, and applications."))
 
-  (:p "Submissions should be uploaded to <a href=\"https://www.easychair.org/conferences/?conf=els2025\">EasyChair</a>.")
+  (:p "Submissions should be uploaded to <a
+  href=\"https://www.easychair.org/conferences/?conf=els2025\">EasyChair</a>. Note: to help us with the review process please indicate the type of submission in the title field in the submission form.")
 
   (:p "LaTeX submissions are strongly encouraged. Please use the LaTeX article template available <a href=\"https://european-lisp-symposium.org/static/submission/template.tex\">here</a> to format your submission." )
 
